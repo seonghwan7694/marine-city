4b18696df7cac5baf9b61113f418ffa7caed8a56
idonotknowwhattodo <seonghwan7694@gmail.com>
2022-11-12 00:24:20 +0900

    자동화를 위한 쉘 스크립트입니다.
    1. 마이그레이션 로그 삭제 및 DB 삭제
    2. 어드민 계정 생성
    3. 로컬 서버 실행
    
    등의 명령어를 포함합니다.

---
+++ django_project/init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# db를 초기화 시켜주고, id : admin 과 pwd : qwert1234인 어드민 계정을 생성하고, 로컬서버를 자동으로 열어주는 쉘 스크립트입니다.
+
+rm -rf base/migrations/*
+touch base/migrations/__init__.py
+rm -rf db.sqlite3
+python manage.py makemigrations
+python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'qwert1234')" | python manage.py shell
+python manage.py runserver
