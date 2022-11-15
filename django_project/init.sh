497e65e0dfebb682601f272a81b7009361485a75
idonotknowwhattodo <seonghwan7694@gmail.com>
2022-11-16 01:02:42 +0900

    [리팩토링 (4-1)] django_project 내 .idea 설정 파일 업로드, 쉘 스크립트 약간의 편집

--- django_project/init.sh
+++ django_project/init.sh
@@ -9,6 +9,6 @@ touch app/migrations/__init__.py
 python manage.py makemigrations
 python manage.py migrate
 
-python manage.py collectstatic
+echo yes | python manage.py collectstatic
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'qwert1234')" | python manage.py shell
 uwsgi --http :8000 --module conf.wsgi
