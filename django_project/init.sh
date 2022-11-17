73b0c56e874b9b2c87542f192919c39e6a59b833
idonotknowwhattodo <seonghwan7694@gmail.com>
2022-11-17 18:43:12 +0900

    [django_project - 2] footer 제작, 디렉터리 구조 변경
    1. BASE_DIR 내 templates/main.html, templates/header, templates/footer.html로 옮김
    2. footer 제작

--- django_project/init.sh
+++ django_project/init.sh
@@ -11,5 +11,5 @@ python manage.py migrate
 
 echo yes | python manage.py collectstatic
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'qwert1234')" | python manage.py shell
-python manage.py runserver
-# uwsgi --http :8000 --module conf.wsgi
+# python manage.py runserver
+uwsgi --http :8000 --module conf.wsgi
